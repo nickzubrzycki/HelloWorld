ce16932e5f6f069333ea4c96b1ca24e1a308e337
nickz <nick.zubrzycki@skynet.be>
2021-02-22 15:40:22 +0100

    HalloWorld 2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,8 +10,34 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+
+            
+            Console.Write("Voornaam: ");
+            string Voornaam;
+            Voornaam = Console.ReadLine();
+            Console.Write("Naam: ");
+            string Naam;
+            Naam = Console.ReadLine();
+            Console.Write("Straat: ");
+            string Straat;
+            Straat = Console.ReadLine();
+            Console.Write("Nummer: ");
+            string Nummer;
+            Nummer = Console.ReadLine();
+            Console.Write("Postcode: ");
+            string Postcode;
+            Postcode = Console.ReadLine();
+            Console.Write("Stad: ");
+            string Stad;
+            Stad = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Naam: " + Voornaam + " " + Naam);
+            Console.WriteLine("Adres: " + Straat + " " + Nummer + ", " + Postcode + " " + Stad);
             Console.ReadLine();
+
+
+
+
         }
     }
 }
